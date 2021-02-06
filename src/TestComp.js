07b8cbabd70c13fa6ae76bb06ace842dea58376e
spap975 <48142512+spap975@users.noreply.github.com>
2021-02-05 21:38:58 -0500

    Css File

--- src/TestComp.js
+++ src/TestComp.js
@@ -1,7 +1,9 @@
+import "./TestComp.css";
+
 function TestComp() {
   return (
     <div>
-    <p>Hello World</p>
+    <p className="title">Hello World</p>
     <button>Press me PLS</button>
     </div>
   );
