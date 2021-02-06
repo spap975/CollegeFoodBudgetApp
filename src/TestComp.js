39b30c355a6fea88d92369cffa950321c72faffc
yousha123 <57017540+yousha123@users.noreply.github.com>
2021-02-06 15:47:53 -0500

    Form Files Done

--- src/TestComp.js
+++ src/TestComp.js
@@ -10,10 +10,19 @@ function TestComp() {
       <p className= "DietaryRestrictions">Dietary Restrictions</p>
       <input type="checkbox" id="Vegetarian" name="Vegetarian" value="Vegetarian"></input>
       <label for="Vegetarian"> Vegetarian</label>
-      <input type="checkbox" id="Gluten" name="Gluten" value="Gluten"></input>
+      <input type="checkbox" id="GlutenFree" name="GlutenFree" value="GlutenFree"></input>
       <label for="Gluten">Gluten Free</label>
       <input type="checkbox" id="Vegan" name="Vegan" value="Vegan"></input>
       <label for="Vegan"> Vegan</label>
+      <input type="checkbox" id="DairyFree" name="DairyFree" value="DairyFree"></input>
+      <label for="Vegetarian"> DairyFree</label>
+      <input type="checkbox" id="Kosher" name="Kosher" value="Kosher"></input>
+      <label for="Kosher"> Kosher</label>
+      <input type="checkbox" id="PeanutAllergy" name="PeanutAllergy" value="PeanutAllergy"></input>
+      <label for="PeanutAllergy"> Peanut Allergy</label>
+
+      <input type="submit" value="Submit">
+
     </form>
     </div>
   );
