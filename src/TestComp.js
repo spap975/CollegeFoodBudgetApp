51935d4a5e418969a551733ba0474881b21340db
yousha123 <57017540+yousha123@users.noreply.github.com>
2021-02-06 15:44:23 -0500

    homepage form halfway

--- src/TestComp.js
+++ src/TestComp.js
@@ -7,6 +7,13 @@ function TestComp() {
     <form>
       <p className= "Budget-title">Budget (per week)</p>
       <input type="text" id="budget" name="fname"></input>
+      <p className= "DietaryRestrictions">Dietary Restrictions</p>
+      <input type="checkbox" id="Vegetarian" name="Vegetarian" value="Vegetarian"></input>
+      <label for="Vegetarian"> Vegetarian</label>
+      <input type="checkbox" id="Gluten" name="Gluten" value="Gluten"></input>
+      <label for="Gluten">Gluten Free</label>
+      <input type="checkbox" id="Vegan" name="Vegan" value="Vegan"></input>
+      <label for="Vegan"> Vegan</label>
     </form>
     </div>
   );
