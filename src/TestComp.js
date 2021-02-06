4de580db80053fb8a262b4b2638c28f31036af1c
yousha123 <57017540+yousha123@users.noreply.github.com>
2021-02-06 15:31:41 -0500

    BUDGET (PERWEEK) ADDED

--- src/TestComp.js
+++ src/TestComp.js
@@ -4,7 +4,10 @@ function TestComp() {
   return (
     <div>
     <p className="title">BUDGET APP</p>
-    <button>Press me PLS</button>
+    <form>
+      <label for="FORM-BUDGET">Budget (per week)</label>
+      <input type="text" id="budget" name="fname"></input>
+    </form>
     </div>
   );
 }
