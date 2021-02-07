a853c28bd1f769e6ded3184a2d3df0985e52d3e5
Emily Miller <emm190@pitt.edu>
2021-02-06 19:24:10 -0500

    Homepage stuff

--- src/BreakdownPage.js
+++ src/BreakdownPage.js
@@ -10,9 +10,20 @@ function BreakdownPage() {
   }
 
   return (
-    
     <div>
-
+      <div className="Title" style={{ background: '#e1f5ba', width: '100%',textAlign: 'center'}}>
+      <h1> Eat well this week!</h1>
+      </div>
+      <div className="row">
+        <div className="column" style={{ background: '#e1f5ba', width: '50%',textAlign: 'center'}}>
+            <h2>Grocery List</h2>
+            <p>Data..</p>
+        </div>
+        <div className="column" style={{ background: '#f1fae1', width: '50%',textAlign: 'center'}}>
+            <h2>Weekly Recipes</h2>
+            <p>Data..</p>
+        </div>
+    </div>
     </div>
     
   );
