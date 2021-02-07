d427a0985d28259e92d6b25f10438ebb550e88be
spap975 <48142512+spap975@users.noreply.github.com>
2021-02-06 20:57:54 -0500

    Update InputPage.js

--- src/InputPage.js
+++ src/InputPage.js
@@ -6,10 +6,14 @@ import DomParser from "dom-parser";
 function InputPage() {
 
   const inputRef = useRef("");
+  const vegetarian = useRef();
+  const vegan = useRef();
+  const gluten = useRef();
+  const dairy = useRef();
+  const peanut = useRef();
 
-  const handleSubmit = () => {
-
-    var parser = new DomParser();
+  const handleSubmit = (props) => {
+  
     // var options = {
     //   method: 'GET',
     //   url: 'https://api.spoonacular.com/recipes/complexSearch',
@@ -29,6 +33,17 @@ function InputPage() {
     //   console.error(error);
     // });
 
+    const userData = {
+      "budget" : inputRef.current.value,
+      "vegetarian" : vegetarian.current.checked,
+      "vegan" : vegan.current.checked,
+      "gluten" : gluten.current.checked,
+      "dairy" : dairy.current.checked,
+      "peanut" : peanut.current.checked
+    };
+
+    window.location = `/Breakdown?budget=${inputRef.current.value}&vege=${vegetarian.current.checked}&v=${vegan.current.checked}&g=${gluten.current.checked}&d=${dairy.current.checked}&p=${peanut.current.checked}`;
+
   }
 
   return (
@@ -46,33 +61,33 @@ function InputPage() {
         <div className="form">
             <label>Dietary Restrictions</label>
             <br></br>
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" ref={vegetarian}></input>
             <label class="form-check-label" for="Vegetarian"> Vegetarian
             </label>
 
             <br></br>
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" ref={vegan}></input>
             <label class="form-check-label" for="Vegan"> Vegan
             </label>
 
             <br></br>
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" ref={gluten}></input>
             <label class="form-check-label" for="GlutenFree"> Gluten Free
             </label>
 
             <br></br>
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" ref={dairy}></input>
             <label class="form-check-label" for="DairyFree"> Dairy Free
             </label>
 
             <br></br>
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" ref={peanut}></input>
             <label class="form-check-label" for="PeanutAllergy"> Peanut Allergy
             </label>
         </div>
 
         <br></br>
-        <button type="submit" className="btn btn-primary btn-block" onClick={handleSubmit}>Submit</button>
+        <button class="btn btn-primary" type="button" onClick={handleSubmit}>Submit</button>
 
     </form>
     </div>
